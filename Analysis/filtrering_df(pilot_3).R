498f0ddc5d8b6020f738db26e56b3808197c8185
iair-embon <iair.embon@gmail.com>
2023-08-16 16:11:10 -0300

    nc

---
+++ Analysis/filtrering_df(pilot_3).R
@@ -0,0 +1,101 @@
+# Here I will read the df and make a filtering based on psychiatric condition and medication
+# After that, I will combine both df in a unique long df
+
+library(dplyr)
+
+# Read the .txt result
+root <- rprojroot::is_rstudio_project
+basename(getwd())
+
+filepath <- root$find_file("pilot_3/dot_morph_first/df_exp_filtered_mod.csv")
+df_exp <- read.csv(file = filepath)
+
+filepath <- root$find_file("pilot_3/dot_morph_first/df_NotExperimentData.csv")
+df_demographic <- read.csv(file = filepath)
+
+## make only one df
+
+df_demographic_colnames <- colnames(df_demographic)
+
+for (column_name in df_demographic_colnames) {
+  df_exp[[column_name]] <-  rep(df_demographic[[column_name]], each= 2) 
+}
+
+
+## save the dataframe
+df_exp_long <- df_exp
+
+filepath <- root$find_file("pilot_3/dot_morph_first/df_exp_long.Rda")
+save(df_exp_long,file = filepath)
+
+# save it in csv
+filepath <- root$find_file("pilot_3/dot_morph_first/df_exp_long.csv")
+write.csv(df_exp_long, file = filepath, row.names = FALSE)
+
+
+## Filter by psychiatric condition and medication
+
+df_demographic_filter <- df_demographic %>%
+  filter(AffectionPsycho_1 == "No") 
+
+df_demographic_filter <- df_demographic_filter %>%
+  filter(medication_1 == "No") 
+
+## filter by data quality question
+df_demographic_filter <- df_demographic_filter %>%
+  filter(QualityTest == "You can count on my answers, I participated in the experiment conscientiously and tried to answer honestly.") 
+
+## filter the participant also in df_exp
+
+df_exp_filter <- df_exp %>%
+  filter((participant %in% df_demographic_filter$participant))
+
+paste("Total participants after filter: ", length(unique(df_exp_filter$participant)))
+
+## save the filter dataframe
+df_exp_filter_long <- df_exp_filter
+
+filepath <- root$find_file("pilot_3/dot_morph_first/df_exp_filter_long.Rda")
+save(df_exp_filter_long,file = filepath)
+
+# save it in csv
+filepath <- root$find_file("pilot_3/dot_morph_first/df_exp_filter_long.csv")
+write.csv(df_exp_filter_long, file = filepath, row.names = FALSE)
+
+## from long to wide data set
+library(tidyverse)
+
+# with out filter
+
+df_exp_wide <- df_exp_long %>%
+  pivot_wider(
+    names_from = position,
+    values_from = c(position,type, response, rt),
+    names_prefix = "trial_"
+  ) %>%
+  select(-starts_with("trial_NA")) %>%
+  distinct(participants, .keep_all = TRUE)
+
+# show new data set wide
+head(df_exp_wide)
+
+
+# with filter
+df_exp_filter_wide <- df_exp_filter_long %>%
+  pivot_wider(
+    names_from = position,
+    values_from = c(position,type, response, rt),
+    names_prefix = "trial_"
+  ) %>%
+  select(-starts_with("trial_NA")) %>%
+  distinct(participants, .keep_all = TRUE)
+
+# show new data set wide
+head(df_exp_filter_wide)
+
+# save both data set
+filepath <- root$find_file("pilot_3/dot_morph_first/df_exp_filter_wide.csv")
+write.csv(df_exp_filter_wide, file = filepath, row.names = FALSE)
+
+filepath <- root$find_file("pilot_3/dot_morph_first/df_exp_wide.csv")
+write.csv(df_exp_wide, file = filepath, row.names = FALSE)
