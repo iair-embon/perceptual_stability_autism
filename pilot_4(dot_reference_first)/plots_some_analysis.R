ea5c0a5a3937856571893199669d0a7346449ebd
iair-embon <iair.embon@gmail.com>
2023-12-14 12:47:19 -0300

    nc

---
+++ pilot_4(dot_reference_first)/plots_some_analysis.R
@@ -0,0 +1,33 @@
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+
+root <- rprojroot::is_rstudio_project
+basename(getwd())
+
+# load dataframe 
+filepath <- root$find_file("pilot_4(dot_reference_first)/df_exp.csv")
+#load(file= filepath)
+df_exp <- read.csv(filepath)
+
+# Calcular el promedio de estimation por tipo usando dplyr
+df_avg <- df_exp %>%
+  group_by(type) %>%
+  summarise(avg_estimation = mean(estimation))
+
+# Crear un gráfico de caja (boxplot)
+boxplot <- ggplot(df_exp, aes(x = type, y = estimation)) +
+  geom_boxplot() +
+  theme_bw()
+
+# Crear un scatterplot con jitter
+scatterplot <- ggplot(df_exp, aes(x = type, y = estimation, group = participants)) +
+  geom_jitter(width = 0.05, alpha = 0.7) +
+  geom_line(aes(group = participants), alpha = 0.5) + 
+  theme_bw()
+
+# Combinar los gráficos usando patchwork (si tienes la librería instalada)
+combined_plot <- boxplot + scatterplot
+combined_plot
+
+
